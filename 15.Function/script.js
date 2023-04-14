ee5f49d9002f43fb391b88f07384b1bfba899c69
BestiSuleymanova <127179716+bestisuleymanova@users.noreply.github.com>
2023-04-14 09:57:25 +0400

    Add files via upload

---
+++ 15.Function/script.js
@@ -0,0 +1,27 @@
+///////////// 1.Return the First Element in an Array.
+
+
+const arr1 = [1, 2, 3];
+const arr2 = [80, 5, 100];
+const arr3 = [-500, 0, 50];
+
+const getFirstValue = function (comple) {
+  return comple[0];
+};
+console.log(getFirstValue(arr1));
+console.log(getFirstValue(arr2));
+console.log(getFirstValue(arr3));
+
+////////////// 2.Return the Last Element in an Array.
+
+
+const arra = [1, 2, 3];
+const arrb = ["cat", "dog", "duck"];
+const arrc = [true, false, true];
+
+const getLastItem = function (any) {
+  return any[2];
+};
+console.log(getLastItem(arra));
+console.log(getLastItem(arrb));
+console.log(getLastItem(arrc));
